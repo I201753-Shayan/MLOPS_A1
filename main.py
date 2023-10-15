eb0bd57a20c4ea1394959251c67daf0bc9de1110
I201753 Shayan <112021217+I201753-Shayan@users.noreply.github.com>
2023-10-15 12:00:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-class Calculator:
-    def __init__(self):
-        self.result = 0
-
-    def add(self, x, y):
-        self.result = x + y
-
-    def subtract(self, x, y):
-        self.result = x - y
-
-    def multiply(self, x, y):
-        self.result = x * y
-
-    def divide(self, x, y):
-        if y == 0:
-            raise ValueError("Division by zero is not allowed.")
-        self.result = x / y
-
-    def get_result(self):
-        return self.result
+def add(a, b):
+    return a + b
