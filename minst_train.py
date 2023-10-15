82a84b3da3e249bfcef048891f8f02014b4529a5
Shayan Bakht <70213048+MS-Bakht@users.noreply.github.com>
2023-10-15 14:04:22 +0500

    Update minst_train.py
    Updating minst_train file to meet flake8 standards,

--- minst_train.py
+++ minst_train.py
@@ -8,7 +8,9 @@ mnist = fetch_openml("mnist_784")
 X, y = mnist["data"], mnist["target"]
 
 # Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size=0.2, random_state=42
+)
 
 model = LogisticRegression(max_iter=100)
 model.fit(X_train, y_train)
