ba2bcb730eb82de7779b92c00dd8ffb8fb16db35
I201753 Shayan <112021217+I201753-Shayan@users.noreply.github.com>
2023-10-15 11:59:47 +0500

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,28 +1,4 @@
-from main import Calculator
+import main
 
-def test_calculator_add():
-    calculator = Calculator()
-    calculator.add(5, 3)
-    assert calculator.get_result() == 8
-
-def test_calculator_subtract():
-    calculator = Calculator()
-    calculator.subtract(10, 4)
-    assert calculator.get_result() == 6
-
-def test_calculator_multiply():
-    calculator = Calculator()
-    calculator.multiply(3, 7)
-    assert calculator.get_result() == 21
-
-def test_calculator_divide():
-    calculator = Calculator()
-    calculator.divide(10, 2)
-    assert calculator.get_result() == 5
-
-def test_calculator_divide_by_zero():
-    calculator = Calculator()
-    try:
-        calculator.divide(5, 0)
-    except ValueError as e:
-        assert str(e) == "Division by zero is not allowed."
+def test_add():
+    assert main.add(2, 3) == 5
